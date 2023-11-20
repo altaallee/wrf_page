d9b32bfad27de7306f012cd33ad927774dbcdfd0
wedge <>
2023-11-19 16:42:22 -0800

    Changed image source to data server.

--- src/MapPannel.js
+++ src/MapPannel.js
@@ -22,7 +22,7 @@ class MapPannel extends React.Component {
                     onNextHourClick={this.props.onNextHourClick}
                     onLastHourClick={this.props.onLastHourClick} />
                 <ImageDisplay
-                    src={"/model_images/images/" + this.props.initTime + "/" + this.props.ens + "/" + this.props.product + "/" + this.props.product + "_" + this.props.domain + "_" + this.props.currentTime + ".png"} />
+                    src={"http://127.0.0.1:8000/wrf/images?domain=" + this.props.domain + "&ens=" + this.props.ens + "&fcst_date=" + this.props.currentTime + "&init_date=" + this.props.initTime + "&product=" + this.props.product} />
             </div>
         )
     }
